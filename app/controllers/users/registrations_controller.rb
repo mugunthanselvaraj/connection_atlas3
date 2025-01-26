0574e5af88d01801380a8052c2c34091f36834ac
mugunthanselvaraj <mailmugunth@gmail.com>
2025-01-26 17:49:15 +0300

    with front end

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -68,19 +68,17 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def respond_with(resource, _opts = {})
     if request.method == "POST" && resource.persisted?
       render json: {
-        status: { code: 200, message: "Signed up sucessfully." },
+        message: "Signed up sucessfully.",
         data: resource,
       }, status: :ok
     elsif request.method == "DELETE"
       render json: {
-        status: { code: 200, message: "Account deleted successfully." },
+        message: "Account deleted successfully.",
       }, status: :ok
     else
       render json: {
-        status: { code: 422,
-                  message: "User couldn't be created successfully.",
-                  errors: "#{resource.errors.full_messages.to_sentence}" },
-      }, status: :unprocessable_entity
+               message: "User couldn't be created successfully. #{resource.errors.full_messages.to_sentence}",
+             }, status: :unprocessable_entity
     end
   end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -31,7 +31,7 @@ class Users::SessionsController < Devise::SessionsController
 
   def respond_with(resource, _opts = {})
     render json: {
-      status: { code: 200, message: "Logged in sucessfully." },
+      message: "Logged in sucessfully.",
       data: resource,
     }, status: :ok
   end
@@ -39,12 +39,10 @@ class Users::SessionsController < Devise::SessionsController
   def respond_to_on_destroy
     if current_user
       render json: {
-        status: 200,
         message: "Logged out successfully",
       }, status: :ok
     else
       render json: {
-        status: 401,
         message: "Couldn't find an active session.",
       }, status: :unauthorized
     end
